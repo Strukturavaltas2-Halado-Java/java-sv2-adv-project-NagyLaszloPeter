be70f85a20cbaabfcd068cb1972fbe1c71fd5624
NagyLP <nagy.peter@pr.hu>
2022-06-15 20:56:49 +0200

    init

---
+++ src/main/java/poster/PostErDemoApplication.java
@@ -0,0 +1,13 @@
+package poster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostErDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostErDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poster/PostErDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package poster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostErDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
